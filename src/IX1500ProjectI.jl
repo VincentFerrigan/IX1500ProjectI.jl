d98bdc2c51a2c653c97601561851aa67f6f4a83b
VincentFerrigan <vincent.ferrigan@gmail.com>
2022-09-13 16:35:42 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/IX1500ProjectI.jl
@@ -0,0 +1,5 @@
+module IX1500ProjectI
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using IX1500ProjectI
+using Test
+
+@testset "IX1500ProjectI.jl" begin
+    # Write your tests here.
+end
