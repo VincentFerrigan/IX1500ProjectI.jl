45c263465e76111cf675b3d82649311fe34f6fcb
VincentFerrigan <vincent.ferrigan@gmail.com>
2022-09-14 09:13:01 +0200

    seperating code from notebook

--- src/IX1500ProjectINotebook.jl
+++ src/IX1500ProjectINotebook.jl
@@ -16,44 +16,6 @@ end
 ### A ProjectOneGroup17.jl notebook ###
 
 # ╔═╡ 7d3d76b4-a16b-4f86-bb72-6e489edd243e
-md"
-# PROJECT-stuff _(To be disabled from the report)_
-## Open Issues
-### Pluto
-- How do I add none code? 
-*Well, just use Markdown with md\" ...\"*
-
-- How do I add keybindings?
-- How do I spellcheck with LT?
-
-## Info & tutorials
-
-### Regular Expressions in Julia
-- [GeeksforGeeks ion regex in Julia](https://www.geeksforgeeks.org/regular-expressions-in-julia/)
-- [Julialangs strings doc](https://docs.julialang.org/en/v1/manual/strings/)
-- [Derek Banas REGEX Turorial](https://www.youtube.com/watch?v=DRR9fOXkfRE&list=PLFA4F2FDD28D0C40E)
-
-### Julia & Pluto
-* [JULIA TUTORIAL at new Think Tank, Derek Bananas](https://www.newthinktank.com/2018/10/julia-tutorial/)
-* [MOOC course](https://syl1.gitbook.io/julia-language-a-concise-tutorial/)
-* [RIP Tutorial](https://riptutorial.com/julia-lang)
-* [CS Lectures](https://www.cs.mcgill.ca/~dprecup/courses/IntroCS/Lectures/)
-* [Julia Talk](https://www.talkjulia.com/)
-* [Combinatorics](https://juliamath.github.io/Combinatorics.jl/dev/)
-* [Julia Language Database JuliaDB - YouTube](https://www.youtube.com/watch?v=pv5zfIs2lyU&list=PLsu0TcgLDUiLfwJipaXOBRqwqZlT4Atfk)
-* [Julia for simple medical statistical analysis](https://www.youtube.com/watch?v=4nPmKG_f8-M&list=PLsu0TcgLDUiIznEhN165XmykqyLgzwY0Y)
-
-### Markdown
-* [markdown guide](https://www.markdownguide.org/)
-* [markdown cheat-sheet](https://www.markdownguide.org/cheat-sheet/)
-
-### Poker
-* [Hand rankings](https://www.pokerstars.se/en/poker/games/rules/hand-rankings/)
-* [Texas hold'em](https://www.pokerstars.se/en/poker/games/texas-holdem/)
-
-### Birthday Paradox
-* [Understanding the Birthday Paradox](https://betterexplained.com/articles/understanding-the-birthday-paradox/)
-"
 
 # ╔═╡ d14e9714-3365-11ed-1125-7be966581a61
 md"
---
+++ src/TaskA.jl
@@ -0,0 +1,79 @@
+# TaskA.jl
+
+module TaskA
+
+import Base.show
+
+export Card, Deck, Hand, fulldeck, getsuit, getcolor, getrank
+
+
+
+# constants
+const RANKS = Set([:ace, :two, :three, :four, :five, :six, :seven, :eight,
+                :nine, :ten, :jack, :queen, :king])
+const SUITS = Set([:♣, :♢, :♡, :♠])
+
+# types
+struct Card
+    rank::Symbol
+    suit::Symbol
+    function Card(r::Symbol, s::Symbol)
+	    # short-circuit conditionals.
+        r in RANKS || throw(ArgumentError("invalid rank: $r"))
+        s in SUITS || throw(ArgumentError("invalid suit: $s"))
+	    new(r, s)
+	end
+end
+
+# caVrd = Card(:ace, :♡)
+# show(card)
+# println(card)
+
+struct Deck
+    cards::Vector{Card}
+    function Deck(cards::Vector{Card})
+        new(cards)
+    end
+end
+
+struct Hand
+    cards::Vector{Card} # or a set?
+    function Hand(cards::Vector{Card})
+        size(cards)[1] == 2|| throw(ArgumentError("only two hands per cards"))
+        new(cards)
+    end
+    # Or should we use tuples or sets instead???????
+    # cards::Set{Card}
+    # function Hand(cards::Set{Card})
+    #     length(cards) != 2 || throw(ArgumentError("only two hands per cards"))
+    #     new(cards)
+    # end
+end
+
+# utils
+
+function fulldeck() 
+    return Card[Card(rank, suit) for suit in SUITS for rank in RANKS]
+end
+
+getsuit(card::Card) = card.suit
+getrank(card::Card) = card.rank
+function getcolor(card::Card)
+    if card.suit == ♣  ||  card.suit == ♠
+        return :black
+    else
+        return :red
+    end
+end
+
+function Base.show(io::IO, card::Card)
+    print(io, card.rank)
+    print(io, card.suit)
+end
+
+# function Base.show(io::IO, hands::Hand)
+#     for cards in hand.cards
+#         print(card)
+#     end
+# end
+end # module
